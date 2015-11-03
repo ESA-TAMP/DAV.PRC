f62e9c16a62db7f2ca222a4713b5fe287c40d362
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2015-11-03 16:23:28 +0100

    Adding vagrant config and shares

---
+++ vagrant/scripts/development_installation.sh
@@ -0,0 +1,51 @@
+#!/bin/sh -e
+
+# allow a custom location to override the default one if needed
+export ROOT=${1:-"/var/dav-prc"}
+
+# Locate sudo (when available) for commands requiring the superuser.
+# Allows setup of a custom autoconf instance located in the non-root user-space.
+SUDO=`which sudo`
+
+# Add CRS 900913 if not present
+if ! grep -Fxq "<900913> +proj=tmerc +lat_0=0 +lon_0=21 +k=1 +x_0=21500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs  <>" /usr/share/proj/epsg ; then
+    $SUDO sh -c 'echo "# WGS 84 / Pseudo-Mercator" >> /usr/share/proj/epsg'
+    $SUDO sh -c 'echo "<900913> +proj=tmerc +lat_0=0 +lon_0=21 +k=1 +x_0=21500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs  <>" >> /usr/share/proj/epsg'
+fi
+
+# 
+cd "$ROOT/"
+$SUDO python setup.py develop
+
+cd "$ROOT/eoxserver"
+$SUDO python setup.py develop --no-deps
+
+# # Configure EOxServer autotest instance
+# cd "$EOX_ROOT/autotest/"
+
+# # Prepare DBs
+# python manage.py syncdb --noinput --traceback
+# python manage.py loaddata auth_data.json range_types.json --traceback
+
+# # Create admin user
+# python manage.py shell 1>/dev/null 2>&1 <<EOF
+# from django.contrib.auth import authenticate
+# from django.contrib.auth.models import User
+# if authenticate(username='admin', password='admin') is None:
+#     User.objects.create_user('admin','office@eox.at','admin')
+# EOF
+
+# # Collect static files
+# python manage.py collectstatic --noinput
+
+# # Create runtime files
+# touch "$EOX_ROOT/autotest/autotest/logs/eoxserver.log"
+
+# # Load the demonstration if not already present
+# SERIES="MER_FRS_1P_reduced_RGB"
+# python manage.py eoxs_id_check "$SERIES" --type DatasetSeries --traceback \
+#     && python manage.py eoxs_collection_create --type DatasetSeries -i "$SERIES" --traceback
+# for TIF in "$EOX_ROOT/autotest/autotest/data/meris/mosaic_MER_FRS_1P_reduced_RGB/"*.tif
+# do
+#     python manage.py eoxs_dataset_register -r RGB -d "$TIF" -m "${TIF//.tif/.xml}" --collection "$SERIES" --traceback
+# done
---
+++ vagrant/scripts/packages.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+
+# Update all the installed packages
+yum update -y
+
+# Install packages
+yum install -y gdal-eox gdal-eox-python postgis proj-epsg \
+               python-django-extensions python-werkzeug
+yum install -y python-lxml mod_wsgi httpd postgresql-server python-psycopg2 pytz
+yum install -y libxml2 libxml2-python mapserver mapserver-python python-pysqlite-eox
+
+# Install some build dependencies
+yum install -y gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel \
+               openssl-devel readline-devel perl wget httpd-devel pixman-devel \
+               sqlite-devel libpng-devel libjpeg-devel libcurl-devel cmake \
+               geos-devel fcgi-devel gdal-eox-devel python-devel \
+               gdal-eox-driver-envisat gdal-eox-driver-netcdf \
+               gdal-eox-driver-openjpeg2
+
+# Attention: Make sure to not install EOxServer from rpm packages!
+# See development_installation.sh for installation.
+
+
+# install recent version of django (1.6, since 1.7+ requires Python 2.7)
+yum install -y python-pip
+
+pip install "django>=1.6,<1.7"
+pip install python-dateutil
