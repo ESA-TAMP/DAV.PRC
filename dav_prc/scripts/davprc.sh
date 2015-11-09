e0f7cd965b8bbcac13088cfc56a2df9c3174c65e
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2015-11-09 16:04:08 +0100

    Adding utility scripts to add new collections from a dir structure

---
+++ dav_prc/scripts/davprc.sh
@@ -0,0 +1,23 @@
+INSTANCE_DIR=/srv/dav-prc/
+WATCH_DIR=/srv/dav-prc/data/
+
+pushd $INSTANCE_DIR
+
+inotifywait -m $WATCH_DIR -e create -e delete -e moved_to -e moved_from |
+
+   while read path action file; do
+
+       if [ $action = "CREATE,ISDIR" ] || [ $action = "MOVED_TO,ISDIR" ] ; then
+           echo "Directory $file created."
+           #python manage.py eoxs_collection_create -i $file
+           #python manage.py eoxs_collection_datasource -i $file -s "$WATCH_DIR/$file/*tif" -t "%(root)s.xml"
+
+           davprc_add_collection.sh $file
+
+       elif [ $action = "DELETE,ISDIR" ] || [ $action = "MOVED_FROM,ISDIR" ] ; then
+           echo "Directory $file deleted."
+           python manage.py eoxs_collection_delete -i $file
+       fi
+   done
+
+popd
---
+++ dav_prc/scripts/davprc_add_collection.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+COLLECTION=$(basename "$1")
+
+numbands=$(cat "$1/DescribeCoverage" | cut -d" " -f11)
+
+create_rangetype_definition() {
+    name=$1
+    count=$2
+
+    echo "[{\"name\": \"${name}\", \"data_type\": \"UInt16\", \"bands\": ["
+    for i in $(seq 1 $count) ; do
+        echo "
+            {
+                \"definition\": \"http://www.opengis.net/def/property/OGC/0/Radiance\",
+                \"description\": \"$name band $i\",
+                \"gdal_interpretation\": \"Undefined\",
+                \"identifier\": \"${name}_${i}\",
+                \"name\": \"${name}_${i}\",
+                \"uom\": \"undefined\",
+                \"nil_values\": []
+            }"
+        if [ "$i" != "$count" ] ; then
+            echo ","
+        fi
+    done
+    echo "]}]"
+}
+
+echo `create_rangetype_definition $COLLECTION $numbands` | python /srv/dav-prc/manage.py eoxs_rangetype_load
+
+python /srv/dav-prc/manage.py eoxs_collection_create -i $COLLECTION
+python /srv/dav-prc/manage.py eoxs_collection_datasource -i MOD07_RETRIEVEDTEMPERATUREPROFILES -s "$1/*/*/*/*/*/*/*.tif"
+
