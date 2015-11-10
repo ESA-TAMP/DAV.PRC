ea481843dced92611f44b20b978f4b0cb787cfec
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2015-11-10 09:22:57 +0100

    Removing print statement

--- setup.py
+++ setup.py
@@ -34,8 +34,6 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-print glob.glob("dav_prc/scripts/*.sh")
-
 setup(
     name="DAV.PRC",
     version="0.0.1",
